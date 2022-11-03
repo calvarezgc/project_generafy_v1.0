cfdc7274f707e28cbf77b79507205bfc5cb125fc
Cristina Alvarez <cristina.alvarezgc@gmail.com>
2022-11-03 17:35:32 +0100

    update navbar

---
+++ scripts/responsive_menu.js
@@ -0,0 +1,19 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close js code
+let metodoArrow = document.querySelector(".metodo-arrow");
+metodoArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+};
